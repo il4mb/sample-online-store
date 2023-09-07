be7a428dd4d105a6b6b2917173d7903503d22ce5
ilham b <durianbohong@gmail.com>
2023-09-07 17:25:57 +0700

    Changes to be committed: 	modified:   .gitignore 	new file:   .vscode/extensions.json 	new file:   README.md 	deleted:    assets/app.js 	deleted:    assets/app.js.map 	deleted:    assets/app.sass 	deleted:    assets/app.ts 	deleted:    assets/css/output.css 	deleted:    assets/input.css 	new file:   env.d.ts 	modified:   index.html 	modified:   package-lock.json 	modified:   package.json 	new file:   public/favicon.ico 	new file:   src/App.vue 	new file:   src/assets/base.css 	new file:   src/assets/logo.svg 	new file:   src/assets/main.css 	new file:   src/components/HelloWorld.vue 	new file:   src/components/TheWelcome.vue 	new file:   src/components/WelcomeItem.vue 	new file:   src/components/icons/IconCommunity.vue 	new file:   src/components/icons/IconDocumentation.vue 	new file:   src/components/icons/IconEcosystem.vue 	new file:   src/components/icons/IconSupport.vue 	new file:   src/components/icons/IconTooling.vue 	new file:   src/main.ts 	new file:   src/router/index.ts 	new file:   src/views/AboutView.vue 	new file:   src/views/HomeView.vue 	deleted:    tailwind.config.js 	new file:   tsconfig.app.json 	modified:   tsconfig.json 	new file:   tsconfig.node.json 	new file:   vite.config.ts

--- assets/app.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const vue_1 = require("vue");
-const options = {};
-(0, vue_1.createApp)(options).mount("#vue-app");
-//# sourceMappingURL=app.js.map
--- assets/app.ts
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-
-const options = {
-   
-}
-
-createApp(options).mount("#vue-app")
