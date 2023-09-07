f744d8d95c0abeeedde0faafd5c3cb5451b79d2b
ilham b <durianbohong@gmail.com>
2023-09-08 00:25:04 +0700

    Changes to be committed: 	new file:   dist/about.81983c08.js 	new file:   dist/about.81983c08.js.map 	new file:   dist/index.5cae8a6f.css 	new file:   dist/index.5cae8a6f.css.map 	new file:   dist/index.81d04770.js 	new file:   dist/index.81d04770.js.map 	new file:   dist/index.99d504d5.js 	new file:   dist/index.99d504d5.js.map 	new file:   dist/index.b7a05eb9.js 	new file:   dist/index.b7a05eb9.js.map 	new file:   dist/index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   src/app.vue 	new file:   src/index.html 	new file:   src/main.ts 	new file:   src/router.ts 	new file:   src/shims-vue.d.ts 	new file:   src/views/about.vue 	new file:   src/views/home.vue 	new file:   src/vite-env.d.ts 	new file:   tsconfig.json 	new file:   vite.config.ts

---
+++ src/router.ts
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from './views/home.vue'
+
+export default createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/', 
+      component: Home,
+    },
+    {
+      path: '/about',
+      component: () => import('./views/about.vue'), 
+    }
+  ],
+})
